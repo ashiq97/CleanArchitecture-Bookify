01002a94542dd99a8fa63f5a7c343fcf7ae63d25
ashiqurrahmankhan <ashiqur.khan@technonext.com>
2025-07-28 18:21:10 +0600

    intial Commit

---
+++ Bookify.Domain/Apartments/Apartment.cs
@@ -0,0 +1,27 @@
+﻿using Bookify.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Apartments
+{
+    public sealed class Apartment : Entity
+    {
+        public Apartment(Guid id) : base(id)
+        {
+        }
+
+        public Name Name { get; set; }
+        public Description Description { get; set; }
+        public Address Address { get; set; }
+        public Money Price { get; set; }
+        //public string PriceCurrecncy { get; set; }
+        public Money CleaningFeeAmount { get; set; }
+        //public decimal CleaningFeeAmount { get; set; }
+        //public string CleaningFeeCurrency { get; set; }
+        public DateTime? LastBookedOnUtc { get; set; }
+        public List<Amenity> Amenities { get; set; } = new();
+    }
+}
