343c4075ac238b7527f46974cd53da3f7f23cdd7
ashiqurrahmankhan <ashiqur.khan@technonext.com>
2025-07-28 22:04:02 +0600

    Add booking

---
+++ Bookify.Domain/Bookings/Booking.cs
@@ -0,0 +1,33 @@
+﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Bookings
+{
+    public sealed class Booking : Entity
+    {
+        public Booking(Guid id):base(id)
+        {
+            
+        }
+
+        public Guid ApartmentId { get;private set; }
+        public Guid UserId { get;private set; }
+        public DateRange Duration { get; private set; }
+        public Money PriceForPeriod { get; private set; }
+        public Money CleaningFee { get; private set; }
+        public Money AmenitiesUpCharge { get; private set; }
+        public Money TotalPrice { get; private set; }
+        public BookingStatus Status { get; private set; }
+        public DateTime CreatedOnUtc { get; private set; }
+        public DateTime? ConfirmedOnUtc { get; private set; }
+        public DateTime? RejectedOnUtc { get; private set; }
+        public DateTime? CompletedOnUtc { get; private set; }
+        public DateTime? CancelledOnUtc { get; private set; }
+
+    }
+}
